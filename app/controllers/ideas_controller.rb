268e29462775f997087be7cc55023bea6aede446
t-iwabuchi <16403.nit@gmail.com>
2017-08-19 15:30:46 +0900

    chapter5

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,12 +1,12 @@
 class IdeasController < InheritedResources::Base
 
-  private
+  def show
+     @idea = Idea.find(params[:id])
+     @comments = @idea.comments.all
+     @comment = @idea.comments.build
+  end
 
-    def show
-    	 @idea = Idea.find(params[:id])
-       @comments = @idea.comments.all
-       @comment = @idea.comments.build
-    end
+  private
 
     def idea_params
       params.require(:idea).permit(:name, :description, :picture)
